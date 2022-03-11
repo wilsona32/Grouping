f1b7ab2562df8dcc918b8d47ae7c53b43f157ae7
Matthew Karloski <matthew.karloski@yahoo.com>
2022-03-11 08:33:32 -0600

    Boarding pass number
    Should generate a random boarding pass number

--- BoardingList/src/Java/Main.java
+++ BoardingList/src/Java/Main.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class Main {
+    ArrayList<Integer> usedNumbers = new ArrayList<Integer>(); //list of used boarding pass numbers
 public static void imp(){
     //variable initialization
     String name;
@@ -8,6 +9,7 @@ public static void imp(){
     String number;
     String gender;
     String age;
+    int boardingnum;
     Scanner input = new Scanner(System.in);
 
     //Basic Display and Inputs
@@ -22,6 +24,32 @@ public static void imp(){
     gender = input.nextLine();
     System.out.println("How about the age?");
     age = input.nextLine();
+    //getting the Boarding number
+    boardingnum = generateBoardingNum();
+}
+
+/*
+Only thing that will need to change in generateBoardingNum is that the arrayList usedNumbers will most
+likely need to read everything on the file with all users' info, getting
+all the boarding pass numbers from that, then doing this method.
+Can't do it yet since idk where the file is yet
+ */
+public static int generateBoardingNum(){
+    Random rand;
+    int passNum = rand.nextInt((9999-1000)+1)+1000; //random int that's 4 digits
+    boolean used = false;
+    for (int i : usedNumbers){ //check to see if it's been used before.
+        if (i == passNum){
+            used = true;
+            break;
+        }
+    }
+    if (!used){ //if not used, add to list
+        usedNumbers.add(passNum);
+        return passNum;
+    }else { //else, try this again and return that number
+        return generateBoardingNum();
+    }
 }
 
 public static void main(String[] args){
