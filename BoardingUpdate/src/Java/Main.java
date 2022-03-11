812387dc9ced656b21e8ad881a4a056116444468
aawilson100@gmail.com <aawilson100@gmail.com>
2022-03-11 15:28:45 -0500

    List Update
    Updated code with File creations for individual and shared Files

--- BoardingUpdate/src/Java/Main.java
+++ BoardingUpdate/src/Java/Main.java
@@ -1,9 +1,17 @@
+import java.io.File;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
-public static void imp(){
+    static ArrayList<Integer> usedNumbers = new ArrayList<Integer>(); //list of used boarding pass numbers
+//runs the prompt for user input, storing all the information in a new human Object.
+    public static void imp(){
     Scanner input = new Scanner(System.in);
     Human human = new Human();
+    int boardingnum;
     //Basic Display and Inputs
     System.out.println("Hello! What is your name?");
     String name = input.nextLine();
@@ -21,28 +29,66 @@ public static void imp(){
     System.out.println("How about the age?");
     String age = input.nextLine();
     human.setAge(age);
+    boardingnum = generateBoardingNum();
+    indFile();
 }
+//creates the file PER USER for each runthrough
+public static void indFile(){
+    Charset utf8 = StandardCharsets.UTF_8;
+    String test = "test";
+    File create = new File("userFile.txt");
+}
+//creates the OVERALL file to keep records of ALL USERS created using this program.
+    public static void sharedFile(){
+        Charset utf8 = StandardCharsets.UTF_8;
+        String test = "test";
+        File create = new File("shareFile.txt");
 
-public static class Human{
+    }
+
+//everything that Is important to the Human object initialization
+public static class Human {
     //variable initialization
     private String name;
     private String email;
     private String number;
     private String gender;
     private String age;
-    public Human(){
+
+    public Human() {
     }
+
     // getters and setters
-    public String getName() {return name;}
-    public void setName(String name) {this.name = name;}
-    public String getEmail() {return email;}
-    public void setEmail(String email) {this.email = email;}
-    public String getNumber() {return number;}
-    public void setNumber(String number) {this.number = number;}
-    public String getGender() {return gender;}
-    public void setGender(String gender) {this.gender = gender;}
-    public String getAge() {return age;}
-    public void setAge(String age) {this.age = age;}
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getNumber() {
+        return number;
+    }
+    public void setNumber(String number) {
+        this.number = number;
+    }
+    public String getGender() {
+        return gender;
+    }
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+    public String getAge() {
+        return age;
+    }
+    public void setAge(String age) {
+        this.age = age;
+    }
 
     public Human(String name, String email, String number, String gender, String age) {
         this.name = name;
@@ -52,6 +98,31 @@ public static class Human{
         this.age = age;
     }
 }
+
+    /*
+    Only thing that will need to change in generateBoardingNum is that the arrayList usedNumbers will most
+    likely need to read everything on the file with all users' info, getting
+    all the boarding pass numbers from that, then doing this method.
+    Can't do it yet since idk where the file is yet
+     */
+    public static int generateBoardingNum(){
+        Random rand;
+        int passNum = rand.nextInt((9999-1000)+1)+1000; //random int that's 4 digits
+        boolean used = false;
+        for (int i : usedNumbers){ //check to see if it's been used before.
+            if (i == passNum){
+                used = true;
+                break;
+            }
+        }
+        if (!used){ //if not used, add to list
+            usedNumbers.add(passNum);
+            return passNum;
+        }else { //else, try this again and return that number
+            return generateBoardingNum();
+        }
+    }
+
 
 public static void main(String[] args){
     imp();
